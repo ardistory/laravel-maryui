75220adf5ec9d32743ebedc73648f8f7523f7c7b
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-07-06 20:25:43 +0700

    Update: login schema

--- app/Livewire/MacAddress.php
+++ app/Livewire/MacAddress.php
@@ -4,9 +4,27 @@ namespace App\Livewire;
 
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Mary\Traits\Toast;
 
 class MacAddress extends Component
 {
+    use Toast;
+
+    public string $username = '';
+    public string $password = '';
+    public bool $isLogin = false;
+
+    public function user(): array
+    {
+        return ['wdcp' => 'xlybzk'];
+    }
+
+    public function loginRb()
+    {
+        $this->isLogin = true;
+        $this->success('Login Success!');
+    }
+
     #[Title('Mac-Address')]
     public function render()
     {
--- resources/views/livewire/mac-address.blade.php
+++ resources/views/livewire/mac-address.blade.php
@@ -2,25 +2,18 @@
     <x-header title="Mac-Address" separator progress-indicator />
 
     <div class="grid grid-cols-12 gap-5">
-        <x-card title="Router Board" class="col-span-6">
-            <x-form wire:submit="save" no-separator>
-                <x-input label="Login" prefix="RB-951" wire:model="name" />
-                <x-input label="Password" wire:model="name" />
+        <x-card title="Router Board" class="col-span-6 {{ $isLogin ? 'hidden' : '' }}">
+            <x-form wire:submit="loginRb" no-separator>
+                <x-input label="Login" prefix="RB-951" wire:model="username" />
+                <x-input type="password" label="Password" wire:model="password" />
 
                 <x-slot:actions>
-                    <x-button label="Connect" class="btn-primary" type="submit" spinner="save" />
+                    <x-button label="Connect" class="btn-primary" type="submit" spinner="loginRb" />
                 </x-slot:actions>
             </x-form>
         </x-card>
-        <x-card title="Router Board" class="col-span-6">
-            <x-form wire:submit="save" no-separator>
-                <x-input label="Login" prefix="RB-951" wire:model="name" />
-                <x-input label="Password" wire:model="name" />
+        <x-card title="{{ $isLogin ? 'Sudah Login' : 'Belum Login' }}" class="col-span-6">
 
-                <x-slot:actions>
-                    <x-button label="Connect" class="btn-primary" type="submit" spinner="save" />
-                </x-slot:actions>
-            </x-form>
         </x-card>
     </div>
 </div>
